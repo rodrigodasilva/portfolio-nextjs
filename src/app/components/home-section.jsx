50dee0bdda264dd7fc14bc1066e9666882d289ab
Rodrigo Ribeiro <ribeiro.rodrigodasilva@gmail.com>
2024-03-22 16:09:52 -0300

    fix: banner aspect ratio

--- src/app/components/home-section.jsx
+++ src/app/components/home-section.jsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 import { ButtonGroupSocialLinks } from '@/components/button-group-social-links'
 import { Button } from '@/components/ui/button'
@@ -6,17 +7,16 @@ import { IconCircleArrowRight } from '@/components/ui/icons'
 import { Separator } from '@/components/ui/separator'
 
 import { cn } from '@/utils/classnames'
-import Image from 'next/image'
 
-function Banner({ className = '', ...props }) {
+function Banner({ className = '' }) {
   return (
     <Image
-      src="/person.webp"
+      src="/person.png"
       alt="Person"
       width={140}
       height={150}
       className={cn(
-        'relative w-full flex items-end justify-center bg-primary/50',
+        'relative w-full flex items-end justify-center bg-primary/50  pt-4 object-cover',
         className
       )}
       priority
@@ -26,7 +26,7 @@ function Banner({ className = '', ...props }) {
 
 export function HomeSection() {
   return (
-    <section className="max-w-screen-md flex gap-4 w-full px-4 sm:px-0">
+    <section className="max-w-screen-md flex gap-4 w-full px-4 md:px-0">
       <div className="flex flex-col gap-6">
         <div className="flex gap-2 justify-between">
           <div className="flex flex-col justify-center gap-2">
@@ -35,7 +35,7 @@ export function HomeSection() {
               Desenvolvedor Web
             </h4>
           </div>
-          <Banner className="flex sm:hidden max-w-[140px] h-[150px] sm:max-w-[260px] sm:h-[280px] rounded-b-[20px] rounded-t-[40px] sm:rounded-b-[40px] sm:rounded-t-[80px]" />
+          <Banner className="flex sm:hidden max-w-[140px] h-[150px] rounded-b-[20px] rounded-t-[40px]" />
         </div>
         <Separator className="bg-border max-w-[100px] w-full" />
 
@@ -56,7 +56,7 @@ export function HomeSection() {
         </div>
       </div>
 
-      <Banner className="hidden sm:flex max-w-[260px] h-[280px] rounded-b-[40px] rounded-t-[80px]" />
+      <Banner className="hidden sm:flex max-w-[240px] h-[270px] rounded-b-[40px] rounded-t-[80px]" />
     </section>
   )
 }
