25e98e97466bf46418030525b0b9c68affc12e53
Rodrigo Ribeiro <ribeiro.rodrigodasilva@gmail.com>
2024-03-22 17:32:59 -0300

    chore: ajust social links

--- src/components/button-group-social-links/button-group-social-links.jsx
+++ src/components/button-group-social-links/button-group-social-links.jsx
@@ -1,18 +1,41 @@
 import { cn } from '@/utils/classnames'
 
-import { IconFacebook, IconInstagram, IconLinkedin } from '../ui/icons'
+import { IconGithub, IconLinkedin } from '../ui/icons'
+
+const SOCIAL_LINKS = [
+  {
+    icon: IconGithub,
+    name: 'Github',
+    url: 'https://github.com/rodrigodasilva'
+  },
+  {
+    icon: IconLinkedin,
+    name: 'Linkedin',
+    url: 'https://www.linkedin.com/in/rodrigo-ribeiro-programador/'
+  }
+]
 
 export function ButtonGroupSocialLinks({ className }) {
   return (
-    <div
+    <ul
       className={cn(
-        'flex gap-6 items-center text-muted-foreground bg-muted w-max p-2 rounded-md',
+        'flex gap-1 items-center text-muted-foreground bg-muted w-max rounded-md px-0.5',
         className
       )}
     >
-      <IconFacebook />
-      <IconInstagram />
-      <IconLinkedin />
-    </div>
+      {SOCIAL_LINKS.map(({ name, icon: Icon, url }) => (
+        <li key={name} className="flex items-center justify-center">
+          <a
+            type="button"
+            className="px-3 py-2 rounded-md hover:text-primary transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
+            href={url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Icon />
+          </a>
+        </li>
+      ))}
+    </ul>
   )
 }
